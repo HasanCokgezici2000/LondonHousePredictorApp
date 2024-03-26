13efdb3b56d518facb902420f64a9096499a8db9
HasanCokgezici2000 <Hasankardilo2000@gmail.com>
2024-03-26 18:15:44 +0000

    Update housepredictor10.py
    changing directory to github link

--- housepredictor10.py
+++ housepredictor10.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 
 ## Deserialise - unpacking pickle
-with open('rf_houses.sav', 'rb') as file: 
+with open('https://github.com/HasanCokgezici2000/LondonHousePredictorApp/blob/main/rf_houses.sav', 'rb') as file: 
     rf_loaded = pickle.load(file) 
 file.close()
 
